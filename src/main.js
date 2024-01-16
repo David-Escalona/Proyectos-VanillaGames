2c3d43601ac24e0295786ca347adc02335c0e214
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-01-16 19:28:22 +0000

    Commit Guardar main.js #14

--- src/main.js
+++ src/main.js
@@ -10,17 +10,19 @@ import './scss/styles.scss'
 
 // Importamos la vista por defecto (que será home)
 async function cargarVista () {
-  const componente = await import('./vistas/homeVista')
+  const componente = await import('./vistas/registroVista')
   const vista = componente.default
   // Inyectamos la vista home
   document.querySelector('main').innerHTML = vista.template
+  // Ejecutamos la lógica de la vista
+  vista.script()
 }
-cargarVista() 
+cargarVista()
 
-//Inyectamos el componente header
+// Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 
-//Inyectamos el componente footer
+// Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
 
 const componente = await import('./vistas/loginVista')
