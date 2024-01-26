449365e55743f583e43048a3e69e81113096100e
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-01-26 13:50:02 +0000

    Cambios en main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { header } from './componentes/header'
 import { footer } from './componentes/footer'
-
+import { enrutador } from './componentes/enrutador'
 // Import our custom CSS
 import './scss/styles.scss'
 
@@ -20,27 +20,6 @@ document.querySelector('header').innerHTML = header.template
 // Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
 
-observadorRutas: () => {
-  document.body.addEventListener('click', event => {
-    // Evitamos que se cargue la página
-    const link = event.target
-    if (link.classList.contains('router-link')) {
-      console.log('router-link')
-      event.preventDefault()
-      // Obtenemos la ruta del enlace sin el .html
-      const href = link.getAttribute('href')
-      // Añadimos la nueva ruta al historial
-
-      // (El método pushState() permite agregar un nuevo estado a la pila del historial del navegador. Esto significa que una nueva entrada de historial se agrega a la pila y la URL del navegador se actualiza sin recargar la página.)
-      window.history.pushState({ path: href }, '', href)
-      // y ejecutamos el router de nuevo para que detecte los cambios con el evento popstate
-      enrutador.router()
-    }
-  })
-
-  // Detectamos cuando alguien navega por el historial con los botones avanzar y retroceder del navegador.
-  window.addEventListener('popstate', (e) => {
-    console.log('evento popstate - Te estás moviendo por el historial')
-    enrutador.router()
-  })
-}
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
