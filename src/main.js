e7277abc906847fa753d19e96ba6fc9f76836139
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-18 18:01:39 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -1,38 +1,25 @@
-// Import all of Bootstrap's JS
-import 'bootstrap'
+import { header } from './componentes/header'
+import { footer } from './componentes/footer'
 
 // Import our custom CSS
-import './scss/style.scss'
-
-// Importamos la Función para detectar eventos al cargar las vistas
-import { enrutador } from '@components/enrutador'
-
-// Import our components
-import { header } from '@components/header'
-import { footer } from '@components/footer'
-import proyectosVista from './vistas/proyectosVista'
-import loginVista from './vistas/loginVista'
-
-import { createClient } from '@supabase/supabase-js'
+import './scss/styles.scss'
 
 // Importamos la vista por defecto (que será home)
-// async function cargarVista () {
-//   const componente = await import('./vistas/homeVista')
-//   const vista = componente.default
-//   // Inyectamos la vista home
-//   document.querySelector('main').innerHTML = vista.template
-//   // Ejecutamos la lógica de la vista
-//   vista.script()
-// }
-// cargarVista()
+async function cargarVista () {
+  const componente = await import('./vistas/registroVista')
+  const vista = componente.default
+  // Inyectamos la vista home
+  document.querySelector('main').innerHTML = vista.template
+  vista.script()
+}
+cargarVista()
 
 // Inyectamos el componente header
-
 document.querySelector('header').innerHTML = header.template
-header.script()
 
 // Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
-enrutador.observadorRutas()
-// Cargamos la página home
-window.location = '#/home'
+
+  // Inyectamos el componente header
+  document.querySelector('header').innerHTML = header.template
+  header.script()
--- src/vistas/homeVista.js
+++ src/vistas/homeVista.js
@@ -5,7 +5,7 @@ export default {
     <h1 class="mt-5 text-center fw-bold" style="font-size: 100px;">Vanilla Games</h1>
     <div class="m-5 mx-auto" style="max-width: 400px">
         <img
-                src="images/logo.svg"
+                src=".images/logo.svg"
                 alt="fpllefia"
                 class="img-fluid"
             />
