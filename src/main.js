9e54b54c8b05b327c1c967e64e7b4a1a85dc5b36
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-19 15:12:17 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
-i// Import our custom CSS
-import './scss/styles.scss'
+import { header } from './componentes/header'
+import { footer } from './componentes/footer'
 
 // Import all of Bootstrap's JS
 import 'bootstrap'
 
-import { header } from './componentes/header'
-import { footer } from './componentes/footer'
+// Import our custom CSS
+import './scss/styles.scss'
 
 // Importamos la Función para detectar eventos al cargar las vistas
 import { enrutador } from './componentes/enrutador'
+// Importamos la vista por defecto (que será home)
+async function cargarVista () {
+  const componente = await import('./vistas/homeVista')
+  const vista = componente.default
+  // Inyectamos la vista home
+  document.querySelector('main').innerHTML = vista.template
+  // Ejecutamos la lógica de la vista
+  vista.script()
+}
+cargarVista()
 
 // Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
-header.script()
+
 // Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
 
 enrutador.observadorRutas()
 // Cargamos la página home
 window.location = '#/home'
+
+// Inyectamos el componente header
+document.querySelector('header').innerHTML = header.template
+header.script()
